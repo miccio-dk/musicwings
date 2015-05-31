221a95af4198bffe001d8963b40b453140d90e08
Riccardo Miccini <riccardo.miccini@gmail.com>
2015-05-31 19:32:03 +0200

    more fix

--- musicwings/settings.py
+++ musicwings/settings.py
@@ -21,7 +21,7 @@ ALLOWED_HOSTS = ['musicwings.herokuapp.com', 'www.musicwin.gs']
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 
